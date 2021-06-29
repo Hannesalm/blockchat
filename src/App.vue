432006b3f422559524455234d5da082202175642
Hannes Alm <hannes@almar.se>
2021-06-29 20:52:06 +0200

    Added input field for send message and fix for export api module

--- src/App.vue
+++ src/App.vue
@@ -20,21 +20,41 @@
           <span aria-hidden="true"></span>
         </a>
       </div>
-
-      <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start"></div>
-      </div>
     </nav>
+    <!-- Message container -->
+    <div class="container is-fluid"></div>
+
+    <div class="container is-fluid send">
+      <div class="field is-grouped">
+        <p class="control is-expanded">
+          <input class="input" type="text" placeholder="Write message..." />
+        </p>
+        <p class="control">
+          <a class="button">
+            Send
+          </a>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import api from './firebase/api';
+
 export default {
   data() {
     return {
       message: true,
       message: 'Blockchat',
+      currentRoom: '',
     };
+  },
+
+  methods: {
+    add() {
+      api.insertData(this.currentRoom);
+    },
   },
 };
 </script>
@@ -44,5 +64,19 @@ export default {
   font-size: 18px;
   font-family: 'Roboto', sans-serif;
   color: #ef3f4e;
+}
+
+.button {
+  color: white;
+  background-color: #ef3f4e;
+}
+
+.send {
+  position: absolute;
+  bottom: 10px;
+}
+
+.block {
+  width: 350px;
 }
 </style>
--- src/firebase/api.js
+++ src/firebase/api.js
@@ -1,3 +1,5 @@
 import { firebase } from './init';
 
-export default api = {};
+let api = {};
+
+export default api;
